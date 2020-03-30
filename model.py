920b125f8070121fbd448e72754d7767db28b943
dsindex <hazzling@gmail.com>
2020-03-30 18:25:57 +0900

    -

--- model.py
+++ model.py
@@ -505,16 +505,24 @@ class TextBertCLS(BaseModel):
         if self.bert_feature_based:
             # feature-based
             with torch.no_grad():
-                bert_outputs = self.bert_model(input_ids=x[0],
-                                               attention_mask=x[1],
-                                               token_type_ids=None if self.config['emb_class'] in ['roberta'] else x[2]) # RoBERTa don't use segment_ids
+                if 'bart' in self.config['emb_class']:
+                    bert_outputs = self.bert_model(input_ids=x[0],
+                                                   attention_mask=x[1])
+                else:
+                    bert_outputs = self.bert_model(input_ids=x[0],
+                                                   attention_mask=x[1],
+                                                   token_type_ids=None if self.config['emb_class'] in ['roberta'] else x[2]) # RoBERTa don't use segment_ids
                 pooled = bert_outputs[1]
         else:
             # fine-tuning
             # x[0], x[1], x[2] : [batch_size, seq_size]
-            bert_outputs = self.bert_model(input_ids=x[0],
-                                           attention_mask=x[1],
-                                           token_type_ids=None if self.config['emb_class'] in ['roberta'] else x[2]) # RoBERTa don't use segment_ids
+            if 'bart' in self.config['emb_class']:
+                bert_outputs = self.bert_model(input_ids=x[0],
+                                               attention_mask=x[1])
+            else:
+                bert_outputs = self.bert_model(input_ids=x[0],
+                                               attention_mask=x[1],
+                                               token_type_ids=None if self.config['emb_class'] in ['roberta'] else x[2]) # RoBERTa don't use segment_ids
             pooled = bert_outputs[1] # first token embedding([CLS]), see BertPooler
             # [batch_size, bert_hidden_size]
         embedded = pooled
