fbbbbe6b15ed111b52ac5b9afb9badc216c1e62d
dsindex <hazzling@gmail.com>
2020-04-21 00:14:29 +0900

    -

---
+++ etc/jemalloc_omp_kmp.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+
+VERBOSE_MODE=0
+
+function error_handler()
+{
+  local STATUS=${1:-1}
+  [ ${VERBOSE_MODE} == 0 ] && exit ${STATUS}
+  echo "Exits abnormally at line "`caller 0`
+  exit ${STATUS}
+}
+trap "error_handler" ERR
+
+PROGNAME=`basename ${BASH_SOURCE}`
+DRY_RUN_MODE=0
+
+function print_usage_and_exit()
+{
+  set +x
+  local STATUS=$1
+  echo "Usage: ${PROGNAME} [-v] [-v] [--dry-run] [-h] [--help]"
+  echo ""
+  echo " Options -"
+  echo "  -v                 enables verbose mode 1"
+  echo "  -v -v              enables verbose mode 2"
+  echo "      --dry-run      show what would have been dumped"
+  echo "  -h, --help         shows this help message"
+  exit ${STATUS:-0}
+}
+
+function debug()
+{
+  if [ "$VERBOSE_MODE" != 0 ]; then
+    echo $@
+  fi
+}
+
+GETOPT=`getopt -o vh --long dry-run,help -n "${PROGNAME}" -- "$@"`
+if [ $? != 0 ] ; then print_usage_and_exit 1; fi
+
+eval set -- "${GETOPT}"
+
+while true
+do case "$1" in
+     -v)            let VERBOSE_MODE+=1; shift;;
+     --dry-run)     DRY_RUN_MODE=1; shift;;
+     -h|--help)     print_usage_and_exit 0;;
+     --)            shift; break;;
+     *) echo "Internal error!"; exit 1;;
+   esac
+done
+
+if (( VERBOSE_MODE > 1 )); then
+  set -x
+fi
+
+
+# template area is ended.
+# -----------------------------------------------------------------------------
+if [ ${#} != 0 ]; then print_usage_and_exit 1; fi
+
+# current dir of this script
+CDIR=$(readlink -f $(dirname $(readlink -f ${BASH_SOURCE[0]})))
+PDIR=$(readlink -f $(dirname $(readlink -f ${BASH_SOURCE[0]}))/..)
+
+# -----------------------------------------------------------------------------
+# functions
+
+function make_calmness()
+{
+    exec 3>&2 # save 2 to 3
+    exec 2> /dev/null
+}
+
+function revert_calmness()
+{
+    exec 2>&3 # restore 2 from previous saved 3(originally 2)
+}
+
+function close_fd()
+{
+    exec 3>&-
+}
+
+function jumpto
+{
+    label=$1
+    cmd=$(sed -n "/$label:/{:a;n;p;ba};" $0 | grep -v ':$')
+    eval "$cmd"
+    exit
+}
+
+
+# end functions
+# -----------------------------------------------------------------------------
+
+
+
+# -----------------------------------------------------------------------------
+# main 
+
+make_calmness
+if (( VERBOSE_MODE > 1 )); then
+    revert_calmness
+fi
+
+export MALLOC_CONF="oversize_threshold:1,background_thread:true,metadata_thp:auto,dirty_decay_ms:9000000000,muzzy_decay_ms:9000000000"
+export OMP_NUM_THREADS=14 && export KMP_AFFINITY=granularity=fine,compact,1,0
+python evaluate.py --config=configs/config-bert-cls.json --bert_output_dir=bert-checkpoint --bert_do_lower_case --device=cpu --num_examples=100 --num_threads=14
+
+close_fd
+
+# end main
+# -----------------------------------------------------------------------------
