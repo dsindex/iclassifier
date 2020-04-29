5a06c5a843dc378833a76ae4e6b5a2e68caf343c
dsindex <hazzling@gmail.com>
2020-04-29 10:47:55 +0900

    -

--- etc/jemalloc_omp_kmp.sh
+++ etc/jemalloc_omp_kmp.sh
@@ -113,11 +113,11 @@ export OMP_NUM_THREADS=14 && export KMP_AFFINITY=granularity=fine,compact,1,0
 #python evaluate.py --data_dir=data/sst2 --device=cpu --num_examples=100 --num_threads=14
 #python evaluate.py --data_dir=data/sst2 --device=cpu --num_examples=100 --num_threads=14 --enable_dqm
 
-#python evaluate.py --config=configs/config-bert-cls.json --bert_output_dir=bert-checkpoint --bert_do_lower_case --device=cpu --num_examples=100 --num_threads=14
-#python evaluate.py --config=configs/config-bert-cls.json --bert_output_dir=bert-checkpoint --bert_do_lower_case --device=cpu --num_examples=100 --num_threads=14 --enable_dqm
+#python evaluate.py --config=configs/config-bert-cls.json --bert_output_dir=bert-checkpoint --device=cpu --num_examples=100 --num_threads=14
+#python evaluate.py --config=configs/config-bert-cls.json --bert_output_dir=bert-checkpoint --device=cpu --num_examples=100 --num_threads=14 --enable_dqm
 
-#python evaluate.py --config=configs/config-bert-cls.json --data_dir=data/sst2 --bert_output_dir=bert-checkpoint --bert_do_lower_case --device=cpu --num_examples=100 --num_threads=14
-python evaluate.py --config=configs/config-bert-cls.json --data_dir=data/sst2 --bert_output_dir=bert-checkpoint --bert_do_lower_case --device=cpu --num_examples=100 --num_threads=14 --enable_dqm
+#python evaluate.py --config=configs/config-bert-cls.json --data_dir=data/sst2 --bert_output_dir=bert-checkpoint --device=cpu --num_examples=100 --num_threads=14
+python evaluate.py --config=configs/config-bert-cls.json --data_dir=data/sst2 --bert_output_dir=bert-checkpoint --device=cpu --num_examples=100 --num_threads=14 --enable_dqm
 
 close_fd
 
