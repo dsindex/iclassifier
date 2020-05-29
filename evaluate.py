6da2ee3dff32ce85417a05fdd1bb03282ae0ac07
dsindex <hazzling@gmail.com>
2020-05-29 11:09:38 +0900

    -

--- evaluate.py
+++ evaluate.py
@@ -96,16 +96,17 @@ def convert_onnx(config, torch_model, x):
                         'segment_ids': {0: 'batch', 1: 'sequence'},
                         'output': {0: 'batch'}}
         
-    torch.onnx.export(torch_model,               # model being run
-                      x,                         # model input (or a tuple for multiple inputs)
-                      opt.onnx_path,             # where to save the model (can be a file or file-like object)
-                      export_params=True,        # store the trained parameter weights inside the model file
-                      opset_version=11,          # the ONNX version to export the model to
-                      do_constant_folding=True,  # whether to execute constant folding for optimization
-                      verbose=True,
-                      input_names=input_names,   # the model's input names
-                      output_names=output_names, # the model's output names
-                      dynamic_axes=dynamic_axes) # variable length axes
+    with torch.no_grad():
+        torch.onnx.export(torch_model,               # model being run
+                          x,                         # model input (or a tuple for multiple inputs)
+                          opt.onnx_path,             # where to save the model (can be a file or file-like object)
+                          export_params=True,        # store the trained parameter weights inside the model file
+                          opset_version=11,          # the ONNX version to export the model to
+                          do_constant_folding=True,  # whether to execute constant folding for optimization
+                          verbose=True,
+                          input_names=input_names,   # the model's input names
+                          output_names=output_names, # the model's output names
+                          dynamic_axes=dynamic_axes) # variable length axes
 
 def check_onnx(config):
     opt = config['opt']
