8245446f6f6f76f3f5350b5341a85c8d0f4da3ce
dsindex <hazzling@gmail.com>
2020-11-16 16:46:44 +0900

    -

--- train.py
+++ train.py
@@ -361,7 +361,7 @@ def train(opt):
 
         # training
         early_stopping = EarlyStopping(logger, patience=opt.patience, measure=opt.measure, verbose=1)
-        local_worse_steps = 0
+        local_worse_epoch = 0
         prev_eval_measure = float('inf') if opt.measure == 'loss' else -float('inf')
         best_eval_measure = float('inf') if opt.measure == 'loss' else -float('inf')
         for epoch_i in range(opt.epoch):
@@ -386,15 +386,15 @@ def train(opt):
             if opt.measure == 'loss': getting_worse = prev_eval_measure <= eval_measure
             else: getting_worse = prev_eval_measure >= eval_measure
             if getting_worse:
-                local_worse_steps += 1
+                local_worse_epoch += 1
             else:
-                local_worse_steps = 0
-            logger.info('Scheduler: local_worse_steps / opt.lr_decay_steps = %d / %d' % (local_worse_steps, opt.lr_decay_steps))
+                local_worse_epoch = 0
+            logger.info('Scheduler: local_worse_epoch / opt.lr_decay_epoch = %d / %d' % (local_worse_epoch, opt.lr_decay_epoch))
             if not opt.use_transformers_optimizer and \
                epoch_i > opt.warmup_epoch and \
-               (local_worse_steps >= opt.lr_decay_steps or early_stopping.step() > opt.lr_decay_steps):
+               (local_worse_epoch >= opt.lr_decay_epoch or early_stopping.step() > opt.lr_decay_epoch):
                 scheduler.step()
-                local_worse_steps = 0
+                local_worse_epoch = 0
             prev_eval_measure = eval_measure
             # end: scheduling
         # for nni
@@ -474,7 +474,7 @@ def get_params():
     parser.add_argument('--eval_and_save_steps', type=int, default=500, help="Save checkpoint every X updates steps.")
     parser.add_argument('--lr', type=float, default=2e-4)
     parser.add_argument('--lr_decay_rate', type=float, default=1.0, help="Disjoint with --use_transformers_optimizer")
-    parser.add_argument('--lr_decay_steps', type=float, default=2, help="Number of decay epoch steps to be paitent. disjoint with --use_transformers_optimizer")
+    parser.add_argument('--lr_decay_epoch', type=float, default=2, help="Number of decay epoch to be paitent. disjoint with --use_transformers_optimizer")
     parser.add_argument('--warmup_epoch', type=int, default=4,  help="Number of warmup epoch steps")
     parser.add_argument('--patience', default=7, type=int, help="Max number of epoch to be patient for early stopping.")
     parser.add_argument('--save_path', type=str, default='pytorch-model.pt')
