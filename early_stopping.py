3bd2e11ff047028d95a67f2563bb931d8d9b923f
dsindex <hazzling@gmail.com>
2020-11-17 17:58:52 +0900

    -

--- early_stopping.py
+++ early_stopping.py
@@ -28,7 +28,7 @@ class EarlyStopping():
         self._value = value
 
     def status(self):
-        self.logger.info('Status: _steps / patience = %d / %d, value = %f' % (self._step, self.patience, self._value))
+        self.logger.info('EaryStopping Status: _steps / patience = %d / %d, value = %f' % (self._step, self.patience, self._value))
 
     def step(self):
         return self._step
