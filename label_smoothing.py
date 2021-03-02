0d9d2df9850892c656ee18985912c5c194b7f43b
dsindex <hazzling@gmail.com>
2021-03-02 13:27:18 +0900

    -

--- label_smoothing.py
+++ label_smoothing.py
@@ -12,14 +12,15 @@ def reduce_loss(loss, reduction='mean'):
 
 
 class LabelSmoothingCrossEntropy(nn.Module):
-    def __init__(self, epsilon: float = 0.1, reduction='mean'):
+    def __init__(self, epsilon: float = 0.1, ignore_index: int = -100, reduction='mean'):
         super().__init__()
         self.epsilon = epsilon
+        self.ignore_index = ignore_index
         self.reduction = reduction
 
     def forward(self, preds, target):
         n = preds.size()[-1]
         log_preds = F.log_softmax(preds, dim=-1)
         loss = reduce_loss(-log_preds.sum(dim=-1), self.reduction)
-        nll = F.nll_loss(log_preds, target, reduction=self.reduction)
+        nll = F.nll_loss(log_preds, target, ignore_index=self.ignore_index, reduction=self.reduction)
         return linear_combination(loss / n, nll, self.epsilon)
