0433ed2099f4c4c72648f320443e4a50c57f9543
dsindex <hazzling@gmail.com>
2021-04-19 17:20:09 +0900

    -

--- util_bert.py
+++ util_bert.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import, division, print_function
-
 # ------------------------------------------------------------------------------ #
 # base code from
 #   https://github.com/huggingface/transformers/blob/master/examples/utils_ner.py
